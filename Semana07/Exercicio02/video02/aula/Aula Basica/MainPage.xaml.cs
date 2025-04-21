3f39b4bbfa3271674655f45968a513e41e7f4220
Joao Victor <juauuu321@gmail.com>
2025-04-21 20:13:07 -0300

    Adiciona pastas Semana07, Semana08 e Semana09

---
+++ Semana07/Exercicio02/video02/aula/Aula Basica/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula_Basica
+{
+    public partial class MainPage : TabbedPage
+    {
+        string nome;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            string endereco = @"<iframe width=""560"" height=""315"" src=""https://www.youtube.com/embed/uyzmSyvRWT8?si=EauzDSLzWt7iYavd"" title=""YouTube video player"" frameborder=""0"" allow=""accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"" referrerpolicy=""strict-origin-when-cross-origin"" allowfullscreen></iframe>";
+            WVVideo.Source = new HtmlWebViewSource
+            {
+                Html = endereco
+            };
+        }
+
+        private async void BTNOla_Clicked(object sender, EventArgs e)
+        {
+            nome = await DisplayPromptAsync("Nome", "Digite seu nome:", "OK");
+            await DisplayAlert("Aviso", "Ola " + nome, "OK");
+        }
+    }
+
+}
